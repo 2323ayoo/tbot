26b5e9eeffc7abf6f49808ddfab5e4aa7e5d0f5a
2323ayoo <142228214+2323ayoo@users.noreply.github.com>
2023-10-13 14:37:47 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+#Discord Configs
+token = "MTA0MDY4NjY1MTI3OTY4Mzc4NQ.G1rIoU.BZBEM_1kHA7WoJGcCoKEkJ9-ghxhGub6oujfZg"
+staff_role = 1143858345653051392 #Staff role: Id of the discord role that will have access to read and write to tickets - Id del ruolo discord che avrà accesso a leggere e scrivere nei ticket 
+logticket = 1162035328442450122 #Log Ticket: Id of the channel where the logs will be sent - Id del canale dove verranno inviati i log
+
+
+#Texts Configs
+logoticket = "https://cdn.discordapp.com/icons/1143857638015258684/3546da26c3237596fce33adf348f0d4a.webp?size=96" 
+authorname = "Bot Service.xyz"
+ticketcategoryname = "Kauf Kategorie"
+embedcolor = 0x13beee #Embed Color: Hex of the color of your embeds (add 0x before the value) https://g.co/kgs/FHysgo- Hex del colore del vostro embed (aggiungi 0x prima del valore) https://g.co/kgs/FHysgo
+ticketauthor = "Kaufen" 
+ticketfooterdev = "Made By BS.xyz team"
+error_dm = "ERROR!"
